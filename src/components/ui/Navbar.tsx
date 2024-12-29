407a5713d7c934bc95d09c49754ae240155f3e09
joshtorie <yitzmeyer@gmail.com>
2024-12-29 21:35:53 +0200

    logo

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Settings } from 'lucide-react';
+import logo from '../../assets/STreet ART TOuRS.png';
 
 export function Navbar() {
   return (
     <nav className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <Link to="/" className="text-xl font-bold text-gray-800">
-            Street Art Tour
+          <Link to="/" className="flex items-center">
+            <img src={logo} alt="Street Art Tours Logo" className="h-10 w-auto" />
           </Link>
           
           <Link 
